ea092f98465372649a272802b0f2b3d44bb7b017
kusumdhingra <kusum.dhingra@birlasoft.com>
2019-11-05 14:33:06 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import {CreateEmployeeComponent} from './create-employee/create-employee.component';
 import {EmployeeListComponent} from './employee-list/employee-list.component';
-import {EmployeeDetailsComponent} from './employee-details/employee-details.component';
+//import {EmployeeDetailsComponent} from './employee-details/employee-details.component';
 import {UpdateEmployeeComponent} from './update-employee/update-employee.component';
 
 
