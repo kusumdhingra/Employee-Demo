4c70288b29ad6eb7e3f76ae58fb0912048a96781
kusumdhingra <kusum.dhingra@birlasoft.com>
2019-11-05 14:38:29 +0530

    3 files modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,7 +1,7 @@
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import {EmployeeDetailsComponent} from './employee-details/employee-details.component';
-import {UpdateEmployeeComponent} from './employee-details.component';
+import {UpdateEmployeeComponent} from './update-employee.component';
 import {NgModule} from '@angular/core';
 import {Routes,RouterModule} from '@angular/router';
 
