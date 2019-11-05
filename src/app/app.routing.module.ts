a5cd3b01404af6b1a9fcc9817873ea5713117bc0
kusumdhingra <kusum.dhingra@birlasoft.com>
2019-11-05 16:37:04 +0530

    1 file modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,8 +2,8 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 //import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 //import {EmployeeDetailsComponent} from './employee-details/employee-details.component';
 //import {UpdateEmployeeComponent} from './update-employee/update-employee.component';
-//import {NgModule} from '@angular/core';
-//import {Routes,RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes,RouterModule} from '@angular/router';
 
 const routes:Routes =[
   {path:'/',redirectTo:'employees',pathMatch:'full'},
